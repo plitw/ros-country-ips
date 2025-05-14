f2b97e9854c8bba79fe5913e550628649699c57f
PowerLi <pccr10001@gmail.com>
2025-05-14 19:51:43 +0800

    fix comment in rsc

--- generate_routeros_lists.py
+++ generate_routeros_lists.py
@@ -38,9 +38,9 @@ def process_apnic_data():
                 cidr = f"{start}/{value}"
                 country_data[cc]["ipv6"].append(cidr)
     
-    return country_data
+    return country_data, registry
 
-def generate_routeros_script(country_code, ip_list, is_ipv6=False):
+def generate_routeros_script(country_code, ip_list, is_ipv6=False, registry=""):
     ip_version = "ipv6" if is_ipv6 else "ipv4"
     filename = f"{OUTPUT_RSC_DIR}/{country_code}_{ip_version}.rsc"
     
@@ -49,14 +49,14 @@ def generate_routeros_script(country_code, ip_list, is_ipv6=False):
     with open(filename, "a") as file:
         file.write(f"# {country_code.upper()} {ip_version} Address List for RouterOS\n")
         file.write(f"# Generated at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-        file.write(f"# Source: APNIC delegated database\n\n")
+        file.write(f"# Source: {registry.upper()} delegated database\n\n")
         
         file.write(f"{cmd_prefix} firewall address-list remove [{cmd_prefix} firewall address-list find list=\"{country_code}_{ip_version}\"]\n\n")
         
         for cidr in ip_list:
             file.write(f"{cmd_prefix} firewall address-list add list=\"{country_code}_{ip_version}\" address={cidr} comment=\"{country_code}_{ip_version}\"\n")
 
-def generate_plain_script(country_code, ip_list, is_ipv6=False):
+def generate_plain_script(country_code, ip_list, is_ipv6=False, registry=""):
     ip_version = "ipv6" if is_ipv6 else "ipv4"
     filename = f"{OUTPUT_TXT_DIR}/{country_code}_{ip_version}"
     with open(filename, "a") as file:
@@ -65,15 +65,15 @@ def generate_plain_script(country_code, ip_list, is_ipv6=False):
 
 
 def main():
-    country_data = process_apnic_data()
+    country_data, registry = process_apnic_data()
     
     for cc, data in country_data.items():
         if data["ipv4"]:
-            generate_routeros_script(cc, data["ipv4"], is_ipv6=False)
-            generate_plain_script(cc, data["ipv4"], is_ipv6=False)
+            generate_routeros_script(cc, data["ipv4"], is_ipv6=False, registry=registry)
+            generate_plain_script(cc, data["ipv4"], is_ipv6=False, registry=registry)
         if data["ipv6"]:
-            generate_routeros_script(cc, data["ipv6"], is_ipv6=True)
-            generate_plain_script(cc, data["ipv6"], is_ipv6=True)
+            generate_routeros_script(cc, data["ipv6"], is_ipv6=True, registry=registry)
+            generate_plain_script(cc, data["ipv6"], is_ipv6=True, registry=registry)
     
     print(f"Generated RouterOS Address Lists for {len(country_data)} countries")
 
